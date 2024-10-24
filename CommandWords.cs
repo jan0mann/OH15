b84aca9915bc5b831d9249e8fefb266d2fc6bdef
jan0mann <jan.ferchof@icloud.com>
2024-10-24 10:30:21 +0200

    whatever to quit

--- CommandWords.cs
+++ CommandWords.cs
@@ -8,7 +8,7 @@ namespace WorldOfZuul
 {
     public class CommandWords
     {
-        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "whatever" };
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quit" };
 
         public bool IsValidCommand(string command)
         {
