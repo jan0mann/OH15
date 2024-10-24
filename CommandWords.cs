a7b26cbf62b443209a3c41ee9fd9e24166b22653
Bartosh2005 <bartorski2005@o2.pl>
2024-10-24 10:18:31 +0200

    whatever

--- CommandWords.cs
+++ CommandWords.cs
@@ -8,7 +8,7 @@ namespace WorldOfZuul
 {
     public class CommandWords
     {
-        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quiet" };
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "whatever" };
 
         public bool IsValidCommand(string command)
         {
