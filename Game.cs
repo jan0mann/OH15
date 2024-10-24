2b21a3547e8cb582bc3980af7e617d83a636c323
noah2564 <noah.bokelmannq@web.de>
2024-10-24 11:22:26 +0200

    office to coral reef

--- Game.cs
+++ Game.cs
@@ -17,7 +17,7 @@
             Room? OilSpill = new("Island 1", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
             Room? PlasticWaste = new("Island 2", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
             Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
-            Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
+            Room? Coral reef = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
 
             MainIsland.SetExits(null, OilSpill, lab, PlasticWaste); // North, East, South, West
 
